8302bc7767f371aaf2a6ec5d4216ae9d03fafa83
kiet-lt-gnk <kiet-lt@genkisystem.com>
2024-01-08 17:29:55 +0700

    Change port

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ http.listen(4000, () => {
 app.use('/api', require('./app/routes/product.routes'))
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
